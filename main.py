942bf6c65345d284f5037735a584e1d1a49067cc
Yusril <yusril886@gmail.com>
2025-07-19 20:12:43 +0800

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
+print(" ****** Ini adalah program python pertama ****** ")
 print("hello world guys")
-
-# angka1 = 3
-# angka2 = 5
-# hasil = angka1 + angka2
-
-# print(" ****** Ini adalah program python pertama ****** ")
-# # print("Masukkan angka pertama: ")
-# # print(hasil)
-
-# nilai1 = int(input("Masukkan angka pertama: "))
-# nilai2 = int(input("Masukkan angka kedua: "))
-# total = nilai1 + nilai2
-
-# input_user = int(input("Berapa hasil Penjumlahan itu: "))
-
-# if input_user == total:
-#     print("Jawaban kamu benar")
-# else:
-#     print("Jawaban kamu salah")
-#     print(f"Jawaban yang benar adalah {total}")
