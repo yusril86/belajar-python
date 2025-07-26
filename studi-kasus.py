44ada66f496495490742e81301a75297904b8c46
Yusril <yusril886@gmail.com>
2025-07-26 11:16:28 +0800

    comment study case

--- studi-kasus.py
+++ studi-kasus.py
@@ -1,13 +1,13 @@
 # print(" ****** Ini adalah program python pertama ****** ")
 # print("hello world guys")
 
-from google import genai
+# from google import genai
 
-client = genai.Client(api_key="AIzaSyCfaYN7iEsQ9Ge7LWjqZJJmxOlmiU_JtW4")
+# client = genai.Client(api_key="AIzaSyCfaYN7iEsQ9Ge7LWjqZJJmxOlmiU_JtW4")
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Apakah kamu tahu siapa yang menemukan bola lampu? ",
-)
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash",
+#     contents="Apakah kamu tahu siapa yang menemukan bola lampu? ",
+# )
 
-print(response.text)
+# print(response.text)
