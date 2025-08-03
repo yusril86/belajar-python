c2a69db73fc37cac82673f1ac1f94d6460c53f6e
Yusril <yusril886@gmail.com>
2025-08-03 15:28:46 +0800

    materi sesi 3

--- main.py
+++ main.py
@@ -1,48 +1,34 @@
-#  ---CARA PERTAMA---
-# print("*** ***")
-# print("---Nama Saya Yusril---")
-# print("*** ***")
- 
-# ////
- 
-#  ---CARA kEDUA---
-# print('''
-# *** ***
-# ---Nama Saya Yusril---
-# *** ***
-#  ''')
- 
-# judul = "Ini angka"
-# nomor1 = 3
-# nomor2 = 3
-# hasil = nomor1 + nomor2
- 
- 
-# print("angka" )
-# print(hasil)
- 
- 
+# angka = 2 
+print("=================================================")
+print("Selamat datang di program kalkulator sederhana!")
+print("=================================================")
+print("Silakan pilih operasi yang ingin dilakukan:")
+print("1. Penjumlahan")
+print("2. Pengurangan")
+print("3. Perkalian")
+print("4. Pembagian")
+operasi = int(input("Pilih operasi matematika: "))
 
- 
-nama = "Yusril"
-kelas = "Informatika A "
- 
-print("*** CARA PERTAMA ***")
-print("---Nama Saya " + nama + " ---")
-print("---Kelas " + kelas + "---")
-print("*** ***")
- 
-print(f'''
-*** CARA KEDUA ***
----Nama Saya {nama}---
----Kelas {kelas}  ---
-*** ***
- ''')
- 
- 
- 
-# print(f'''
-# *** ***
-# ---Nama Saya {nama}---
-# *** ***
-#  ''')
+angka= int(input("Masukkan sebuah angka: "))
+angka2 = int(input("Masukkan angka kedua: "))
+
+
+
+# print("Angka yang dimasukkan adalah:", angka, "dan", angka2)
+# print(f"ini adalah angka" + str(angka) + " dan " + str(angka2))
+
+
+if operasi == 1:
+    hasil = angka + angka2
+    print("Hasil penjumlahan:", hasil)
+elif operasi == 2:
+    hasil = angka - angka2
+    print("Hasil pengurangan:", hasil)
+elif operasi == 3:
+    hasil = angka * angka2
+    print("Hasil perkalian:", hasil)
+elif operasi == 4: 
+        hasil = angka / angka2
+        print("Hasil pembagian:", hasil) 
+else:
+    print("Operasi Matematika tidak valid. Silakan pilih 1, 2, 3, atau 4.")
